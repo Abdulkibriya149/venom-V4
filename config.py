89ad40efb367dce88e3b2e359fcebc469a43237d
Abdulkibriya149 <abdulkibriya43@gmail.com>
2024-07-19 12:21:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7459905641:AAGk5_L0xsuv6fCVYk5YNXTXjKlv74YVJjQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("DARKxBAN")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5337384634")
 
 
 
